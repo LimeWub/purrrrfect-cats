da62ac4ea1cab2c017d4dadf13147fc897317cba
LimeWub <myrto.kontouli@gmail.com>
2026-02-03 11:34:47 +0000

    refactor: delete unused file

--- src/hooks/useImages.ts
+++
@@ -1,4 +0,0 @@
-// This file is kept for backwards compatibility
-// Use useSearchImages.ts and useUserUploadedImages.ts instead
-export { useSearchImages } from './useSearchImages'
-export { useUserUploadedImages } from './useUserUploadedImages'
